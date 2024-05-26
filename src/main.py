803e1adbeeb54bff8f808816d00f1f6b1379cf0c
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2024-05-26 22:26:23 +0500

    Registered basic msg handler in main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import uvloop
 from pyrogram import idle
+from pyrogram.handlers import MessageHandler
+from typing import Callable
 
 from src.loggers import logger
 from src.custom_client import CustomClient
+from src.message_emoji_responder import MessageEmojiResponder
 from src.user_settings import UserSettings
 
-uvloop.install()  # https://docs.pyrogram.org/topics/speedups
+
+def register_msg_handler(custom_client: CustomClient, func: Callable):
+    pyrogram_response_handler: MessageHandler = MessageHandler(func)
+    custom_client.add_handler(pyrogram_response_handler)
 
 
 user_settings: UserSettings = UserSettings.from_config(config_file="./config.yaml")
+uvloop.install()  # https://docs.pyrogram.org/topics/speedups
 client: CustomClient = CustomClient(name="my_app", user_settings=user_settings)
+register_msg_handler(custom_client=client, func=MessageEmojiResponder.respond)
 
 
 async def main():

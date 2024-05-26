588d67679c737131173308e8eedec3b4465a0bad
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2024-05-27 03:21:04 +0500

    Renamed message_emoji_manager.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from typing import Callable
 
 from src.loggers import logger
 from src.custom_client import CustomClient
-from src.message_emoji_responder import MessageEmojiResponder
+from src.message_emoji_manager import MessageEmojiManager
 from src.user_settings import UserSettings
 
 
@@ -20,7 +20,7 @@ def register_msg_handler(custom_client: CustomClient, func: Callable) -> None:
 user_settings: UserSettings = UserSettings.from_config(config_file="./config.yaml")
 uvloop.install()  # https://docs.pyrogram.org/topics/speedups
 client: CustomClient = CustomClient(name="my_app", user_settings=user_settings)
-register_msg_handler(custom_client=client, func=MessageEmojiResponder.respond)
+register_msg_handler(custom_client=client, func=MessageEmojiManager.respond)
 
 
 async def main():

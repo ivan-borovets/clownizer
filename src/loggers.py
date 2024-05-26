5aba8a96415b1705f3d7a17516c69ffeb3cdc518
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2024-05-26 21:34:06 +0500

    Add logger (loguru)

---
+++ src/loggers.py
@@ -0,0 +1,55 @@
+import sys
+from loguru import logger
+
+
+class LoggerFilters:
+    @staticmethod
+    def success_filter(
+        record: dict,
+    ) -> bool:  # Record type from loguru can't be used for annotation
+        """
+        Filters Loguru Records by condition
+        """
+        return record["level"].name == "SUCCESS"
+
+    @staticmethod
+    def error_filter(
+        record: dict,
+    ) -> bool:  # Record type from loguru can't be used for annotation
+        """
+        Filters Loguru Records by condition
+        """
+        return record["level"].name == "ERROR"
+
+    @staticmethod
+    def success_error_filter(
+        record: dict,
+    ) -> bool:  # Record type from loguru can't be used for annotation
+        """
+        Filters Loguru Records by condition
+        """
+        return record["level"].name in ("SUCCESS", "ERROR")
+
+
+logger.remove()
+logger.add(
+    sink=sys.stderr,
+    format="<blue>{time:YYYY-MM-DD|HH:mm:ss}|</blue><green>{level}</green><blue>|{message}</blue>",
+    colorize=True,
+    level="SUCCESS",
+    filter=LoggerFilters.success_filter,
+)
+logger.add(
+    sink=sys.stderr,
+    format="<yellow>{time:YYYY-MM-DD|HH:mm:ss}|</yellow><red>{level}</red><yellow>|{message}</yellow>",
+    colorize=True,
+    level="ERROR",
+    filter=LoggerFilters.error_filter,
+)
+logger.add(
+    sink="../logs/logfile_{time:YYYY-MM-DD_HH-mm-ss}.log",
+    format="{time:YYYY-MM-DD|HH:mm:ss}|{level}|{message}",
+    filter=LoggerFilters.success_error_filter,
+    rotation="1 day",
+    compression="zip",
+)

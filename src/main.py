1eb70c4369a30d4efba61cbfcd4ed856cae9e4c2
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2024-05-28 18:33:35 +0500

    Register scheduler in main.py with update emoji manager function

--- src/main.py
+++ src/main.py
@@ -17,6 +17,20 @@ def register_msg_handler(custom_client: CustomClient, func: Callable) -> None:
     custom_client.add_handler(pyrogram_response_handler)
 
 
+def register_scheduler(custom_client: CustomClient, func: Callable) -> None:
+    """
+    Registers scheduler with a given function in a provided client
+    """
+    custom_client.scheduler.add_job(
+        func=func,
+        trigger=custom_client.scheduler.trigger,
+        args=[custom_client],
+        id=custom_client.name,
+        replace_existing=True,
+    )
+    custom_client.scheduler.start()
+
+
 user_settings: UserSettings = UserSettings.from_config(config_file="./config.yaml")
 uvloop.install()  # https://docs.pyrogram.org/topics/speedups
 client: CustomClient = CustomClient(
@@ -30,6 +44,7 @@ async def main():
         await client.set_emoticon_picker()
         logger.success("Telegram auth completed successfully!")
         register_msg_handler(custom_client=client, func=message_emoji_manager.respond)
+        register_scheduler(custom_client=client, func=message_emoji_manager.update)
         await idle()
 
 
